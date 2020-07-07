7e372329004958ed0cb1eec08aedb143f8115a33
DillonBarker <barker.dill98@gmail.com>
2020-07-07 16:32:23 +0100

    part 8, adding respond to and release bike to spec file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,5 @@
 require './docking_station.rb'
 
 describe DockingStation do
-  # it 'release bike' do
-  #   expect(docking_station.release). to eq bike
-  # end
+  it { is_expected.to respond_to :release_bike }
 end
